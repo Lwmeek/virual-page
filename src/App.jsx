5255f5fb922dd13b767bfeddb745c1139979095f
Timic Ivey <timicivey27@gmail.com>
2024-05-02 09:28:14 -0400

    creating and finishing feature section component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import Navbar from "./Components/Navbar";
 import HeroSection from "./Components/HeroSection";
+import FeaturesSection from "./Components/FeaturesSection";
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Navbar />
 			<div className="max-w-7xl mx-auto pt-20 px-6">
 				<HeroSection />
+        <FeaturesSection />
 			</div>
 		</>
 	);
