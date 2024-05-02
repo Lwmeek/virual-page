b403d5ce83d631cdb4f8e695824b56432daf702f
Timic Ivey <timicivey27@gmail.com>
2024-05-02 11:48:41 -0400

    creating and finishing workflow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import "./App.css";
 import React from "react";
-import Navbar from "./Components/Navbar";
-import HeroSection from "./Components/HeroSection";
+import "./App.css";
 import FeaturesSection from "./Components/FeaturesSection";
+import HeroSection from "./Components/HeroSection";
+import Navbar from "./Components/Navbar";
+import Workflow from "./Components/Workflow";
 
 const App = () => {
 	return (
@@ -11,6 +12,7 @@ const App = () => {
 			<div className="max-w-7xl mx-auto pt-20 px-6">
 				<HeroSection />
         <FeaturesSection />
+        <Workflow />
 			</div>
 		</>
 	);
--- src/Components/FeaturesSection.jsx
+++ src/Components/FeaturesSection.jsx
@@ -1,8 +1,6 @@
 import { PhoneCall } from "lucide-react";
 import { features } from "../constants";
 
-
-
 const FeaturesSection = () => {
 	return (
 		<div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
@@ -17,21 +15,23 @@ const FeaturesSection = () => {
 					</span>
 				</h2>
 			</div>
-            <div className="flex flex-wrap mt-10 lg:mt-20">
-                {features.map((feature, index) => (
-                    <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
-                        <div className="flex">
-                            <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
-                                {feature.icon}
-                            </div>
-                            <div>
-                                <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
-                                <p className="text-md p-2 mb-20 text-neutral-500">{feature.description}</p>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
+			<div className="flex flex-wrap mt-10 lg:mt-20">
+				{features.map((feature, index) => (
+					<div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+						<div className="flex">
+							<div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
+								{feature.icon}
+							</div>
+							<div>
+								<h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+								<p className="text-md p-2 mb-20 text-neutral-500">
+									{feature.description}
+								</p>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
