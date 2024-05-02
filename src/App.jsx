22985daefd44227f268fe0f2e7dbdfc876d66e2a
Timic Ivey <timicivey27@gmail.com>
2024-05-02 09:05:41 -0400

    craeted and completed Hero section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
+			<div className="max-w-7xl mx-auto pt-20 px-6">
+				<HeroSection />
+			</div>
 		</>
 	);
 };
